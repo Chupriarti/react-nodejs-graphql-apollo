659525453fa769c05645a28331a72441489fe647
Chupriarti <wallx13@gmail.com>
2022-01-13 23:13:16 +0300

    prepare graphql

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}));
 
 app.listen(5000, () => console.log('Server started at 5000'));
