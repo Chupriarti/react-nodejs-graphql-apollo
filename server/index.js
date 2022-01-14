cef1907d9aa90ee8be30a7b9a9541176f2c9abc6
Chupriarti <wallx13@gmail.com>
2022-01-14 22:00:19 +0300

    prepare mock server data

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const cors = require('cors');
 const schema = require('./schema');
+const users = [];
 
 const app = express();
 app.use(cors());
