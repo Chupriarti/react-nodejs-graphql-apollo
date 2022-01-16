50269a4a656ae2024c3c5959382a7a1f499a5b7a
Chupriarti <wallx13@gmail.com>
2022-01-16 23:27:59 +0300

    add create user logic to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,36 @@
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import React from 'react';
 import './app.css'
+import { CREATE_USER } from './mutations/user';
 import { GET_ALL_USERS } from './query/user';
 
 function App() {
   const {data, loading, error} = useQuery(GET_ALL_USERS);
+  const [newUser] = useMutation(CREATE_USER);
   const [users, setUsers] = React.useState([]);
   const [username, setUsername] = React.useState("");
-  const [age, setAge] = React.useState("");
+  const [age, setAge] = React.useState(0);
 
   React.useEffect(() => {
     if (!loading) {
       setUsers(data.getAllUsers);
     }
   }, [data]);
+
+  const addUser = (e) => {
+    e.preventDefault()
+    newUser({
+      variables: {
+        input: {
+          username, age
+        }
+      }
+    }).then(({data}) => {
+      console.log(data)
+      setUsername('')
+      setAge(0)
+    })
+  }
 
   if (loading){
     return (
@@ -27,7 +44,7 @@ function App() {
         <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" />
         <input value={age} onChange={(e) => setAge(e.target.value)} type="number" />
         <div className='btns'> 
-          <button>Add</button>
+          <button onClick={(e) => addUser(e)}>Add</button>
           <button>Get</button>
         </div>
       </form>
